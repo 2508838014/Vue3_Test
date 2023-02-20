2cc349af4e047b76540e20798a7e09bbfa3d8307
续鼎 谢 <2508838014@qq.com>
2023-02-20 15:19:13 +0800

    1.0.1补全一部分功能

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,46 +1,45 @@
 
-<script setup lang="ts">
-    import { RouterLink, RouterView } from 'vue-router'
-    import sidebarVue from "./Sidebar.vue";
-    import Navbar from "./Navbar.vue";
-/*    import HelloWorld from './components/HelloWorld.vue'*/
-    /*   import router from './router'*/
-    import { useRouter } from 'vue-router'
-    const router = useRouter()
-    const toPage = () => {
-        console.log(router)
-
-        router.push('/side')
-    }
-
-
-</script>
 <template>
-      <navbar></navbar>
-  <sidebarVue></sidebarVue>
-    <router-view #default="{ Component }">
-    <transition
-      name="fade"
-      enter-active-class="animate__animated animate__fadeIn"
-      leave-active-class="animate__animated animate__fadeOut"
-    >
-      <component :is="Component" />
-    </transition>
-  </router-view>
-<!--  <main>
-    <TheWelcome />
-  </main>-->
+  <navbar></navbar>
+  <div>
+    <sidebarVue></sidebarVue>
+    <div class="main">
+      <router-view #default="{ Component }">
+        <transition name="fade" enter-active-class="animate__animated animate__fadeIn"
+          leave-active-class="animate__animated animate__fadeOut">
+          <component :is="Component" />
+        </transition>
+      </router-view>
+    </div>
+  </div>
 </template>
+
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import sidebarVue from "./Sidebar.vue";
+import Navbar from "./Navbar.vue";
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const toPage = () => {
+  console.log(router)
+
+  router.push('/side')
+}
+</script>
+
+
 
 <style scoped>
 .main {
-  position: absolute;
+  /* position: absolute; */
+  position: fixed;
+  display: inline;
   margin-top: 100px;
   margin-left: 260px;
-  padding: 40px 0;
   width: calc(100% - 260px);
   height: calc(100% - 100px);
-  background-color: brown;
+  background-color: #CFD3DC;
   /* // background: #000; */
 }
 </style>
